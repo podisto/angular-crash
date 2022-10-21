0d4370d279f4f0d582dc166b0fa9087c3ae240d0
ElHadji Omar DIONE <omardione1991@gmail.com>
2022-10-21 19:23:06 -0400

    initial commit

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+
+  @Input() task: Task
+  faTimes = faTimes
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task: Task) {
+    console.log(`Delete task ${JSON.stringify(task)}`);
+
+  }
+
+}
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Task } from '../Task'
+import { Observable } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private apiUrl: string = "http://localhost:5000/tasks"
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl)
+  }
+}
